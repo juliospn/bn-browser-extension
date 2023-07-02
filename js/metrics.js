4d7adfb4cdc1bbeeff10309be17b50591b2797c8
juliospn <juliospn@gmail.com>
2023-07-02 11:56:51 -0300

    some widget updates

--- js/metrics.js
+++ js/metrics.js
@@ -1,7 +1,7 @@
 const wst = new WebSocket('wss://fstream.binance.com/ws/btcusdt@markPrice@1s');
 const priceDisplay = document.getElementById('price');
 
-wst.onmessage = function(event) {
+wst.onmessage = function (event) {
   const parsedData = JSON.parse(event.data);
   const markPrice = parsedData.p;
 
@@ -24,10 +24,10 @@ function getPriceChange() {
   var url = "https://api.coincap.io/v2/assets/bitcoin";
 
   fetch(url)
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       let changePercent24Hr = parseFloat(data.data.changePercent24Hr);
       let priceChange24Value = document.getElementById("price-change-24-value");
       priceChange24Value.textContent = `${changePercent24Hr.toFixed(2)}%`;
@@ -72,12 +72,49 @@ function getLightningMetrics() {
       lightningChannelsValue.style.display = "block";
 
       const formattedNetworkCapacity = formatNetworkCapacity(data.networkcapacity);
-      networkCapacityValue.textContent = formattedNetworkCapacity;
+      const [value, unit] = formattedNetworkCapacity.split(" "); 
+      networkCapacityValue.innerHTML = `<span class="value">${value}</span> <span class="unit">${unit}</span>`;
       networkCapacityValue.style.display = "block";
     });
 }
 
 getLightningMetrics();
+
+
+
+function getWeekPriceChange() {
+  var url = "https://api.coinranking.com/v2/coin/Qwsogvtv82FCd?timePeriod=7d";
+
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      if (data.status === "success" && data.data.coin) {
+        let priceChange7d = parseFloat(data.data.coin.change);
+        let priceChange7dValue = document.getElementById("price-change-7d-value");
+
+        if (!isNaN(priceChange7d)) {
+          priceChange7dValue.textContent = `${priceChange7d.toFixed(2)}%`;
+          priceChange7dValue.style.display = "block";
+        } else {
+          priceChange7dValue.textContent = "N/A";
+          priceChange7dValue.style.display = "block";
+        }
+      } else {
+        console.error("Invalid data structure:", data);
+      }
+    })
+    .catch(function(error) {
+      console.error("Failed to fetch data:", error);
+    });
+}
+
+getWeekPriceChange();
+
+
+
+
 
 
 
